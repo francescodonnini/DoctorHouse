f8726f509b0ff594a07b52c8049cf72e5f2bffc9
Emanuele <egentili2000@gmail.com>
2023-01-05 18:05:55 +0100

    Added logic control to the registration controller.

--- src/main/java/ispw/uniroma2/doctorhouse/auth/registration/RegisterUserJFX.java
+++ src/main/java/ispw/uniroma2/doctorhouse/auth/registration/RegisterUserJFX.java
@@ -4,10 +4,15 @@ import ispw.uniroma2.doctorhouse.auth.beans.EmailBean;
 import ispw.uniroma2.doctorhouse.auth.beans.GenderBean;
 import ispw.uniroma2.doctorhouse.auth.beans.UserRegistrationRequestBean;
 import ispw.uniroma2.doctorhouse.auth.exceptions.EmailNotValid;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.util.StringConverter;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 public class RegisterUserJFX extends RegisterUserGraphicController {
     @FXML
@@ -99,33 +104,100 @@ public class RegisterUserJFX extends RegisterUserGraphicController {
         nameRequiredLbl.visibleProperty().bind(nameRequiredLbl.managedProperty());
         emailErrorLbl.managedProperty().bind(emailErrorLbl.textProperty().isNotEmpty());
         emailErrorLbl.visibleProperty().bind(emailErrorLbl.managedProperty());
+        passwordErrorLbl.managedProperty().bind(passwordErrorLbl.textProperty().isNotEmpty());
+        passwordErrorLbl.visibleProperty().bind(passwordErrorLbl.managedProperty());
+        lastNameRequiredLbl.managedProperty().bind(lastNameRequiredLbl.textProperty().isNotEmpty());
+        lastNameRequiredLbl.visibleProperty().bind(lastNameRequiredLbl.managedProperty());
+        fiscalCodeErrorLbl.managedProperty().bind(fiscalCodeErrorLbl.textProperty().isNotEmpty());
+        fiscalCodeErrorLbl.visibleProperty().bind(fiscalCodeErrorLbl.managedProperty());
+        confirmPasswordErrorLbl.managedProperty().bind(confirmPasswordErrorLbl.textProperty().isNotEmpty());
+        confirmPasswordErrorLbl.visibleProperty().bind(confirmPasswordErrorLbl.managedProperty());
+        birthDateErrorLbl.managedProperty().bind(birthDateErrorLbl.textProperty().isNotEmpty());
+        birthDateErrorLbl.visibleProperty().bind(birthDateErrorLbl.managedProperty());
     }
 
     @FXML
     void register(ActionEvent event) {
-        boolean cannotSubmit = false;
+        BooleanProperty cannotSubmit = new SimpleBooleanProperty();
+        cannotSubmit.bind(nameRequiredLbl.textProperty().isNotEmpty()
+                .and(passwordErrorLbl.textProperty().isNotEmpty())
+                .and(lastNameRequiredLbl.textProperty().isNotEmpty())
+                .and(emailErrorLbl.textProperty().isNotEmpty())
+                .and(fiscalCodeErrorLbl.textProperty().isNotEmpty())
+                .and(confirmPasswordErrorLbl.textProperty().isNotEmpty())
+                .and(birthDateErrorLbl.textProperty().isNotEmpty())
+                );
+        //get text fields content
         String name = nameTxtFld.getText();
+        String password = passwordTxtFld.getText();
+        String lastName = lastNameTxtFld.getText();
+        String email = emailTxtFld.getText();
+        String fiscalCode = fiscalCodeTxtFld.getText();
+        String confirmPassword = confirmPasswordTxtFld.getText();
+        LocalDate dataPicker = birthDatePicker.getValue();
+        //Check name text field content
         if (name.trim().isEmpty()) {
             nameRequiredLbl.textProperty().set("This field is required!");
-            cannotSubmit = true;
         } else {
             nameRequiredLbl.textProperty().set("");
         }
-        String email = emailTxtFld.getText();
+        //Check email text field content
         if (email.trim().isEmpty()) {
             emailErrorLbl.textProperty().set("This field is required");
-            cannotSubmit = true;
         } else {
             emailErrorLbl.textProperty().set("");
         }
+
         EmailBean emailBean = new EmailBean();
         try {
             emailBean.setEmail(email);
+            emailErrorLbl.textProperty().set("");
+
         } catch (EmailNotValid e) {
             emailErrorLbl.textProperty().set("Invalid email!");
-            cannotSubmit = true;
+
         }
-        if (!cannotSubmit) {
+        //check password text field content
+        if (password.isEmpty()) {
+            passwordErrorLbl.textProperty().set("This field is required");
+        } else {
+            passwordErrorLbl.textProperty().set("");
+        }
+
+        //check lastName text field content
+        if (lastName.trim().isEmpty()) {
+            lastNameRequiredLbl.textProperty().set("This field is required");
+        } else {
+            lastNameRequiredLbl.textProperty().set("");
+        }
+        //check fiscalCode text field content
+        if (fiscalCode.trim().isEmpty()) {
+            fiscalCodeErrorLbl.textProperty().set("This field is required");
+
+        } else {
+            fiscalCodeErrorLbl.textProperty().set("");
+        }
+        //check confirmPassword text field content
+        if (confirmPassword.isEmpty() || !password.equals(confirmPassword)) {
+            confirmPasswordErrorLbl.textProperty().set("Password do not match");
+
+        } else {
+            confirmPasswordErrorLbl.textProperty().set("");
+        }
+        //check dataPicker content
+        if (dataPicker != null) {
+            Period period = Period.between(dataPicker, LocalDate.now().plusDays(1));
+            if (period.getYears() < 18) {
+                birthDateErrorLbl.textProperty().set("You must be at least 18 years old");
+
+            } else {
+                birthDateErrorLbl.textProperty().set("");
+            }
+        } else {
+            birthDateErrorLbl.textProperty().set("This field is required");
+
+        }
+        if (!cannotSubmit.get()) {
             UserRegistrationRequestBean request = new UserRegistrationRequestBean();
             request.setFirstName(name);
             request.setEmail(emailBean);

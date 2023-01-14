da9fd43dd5003b0ca34897acda3691f281b8d0ea
francesco donnini <donnini.francesco00@gmail.com>
2023-01-14 18:13:59 +0100

    Fixed a bug in IrrecoverableError.java

--- src/main/java/ispw/uniroma2/doctorhouse/IrrecoverableError.java
+++ src/main/java/ispw/uniroma2/doctorhouse/IrrecoverableError.java
@@ -1,14 +1,7 @@
 package ispw.uniroma2.doctorhouse;
 
 public class IrrecoverableError extends Error {
-    private final Exception cause;
-
     public IrrecoverableError(Exception cause) {
-        this.cause = cause;
-    }
-
-    @Override
-    public Exception getCause() {
-        return cause;
+        super(cause);
     }
 }

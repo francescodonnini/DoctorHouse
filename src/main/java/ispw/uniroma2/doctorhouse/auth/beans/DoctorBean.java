281a4ce2d08aa521ab342b550306496838c24271
francesco donnini <donnini.francesco00@gmail.com>
2023-01-16 09:43:03 +0100

    Added files for sonar cloud analysis

---
+++ src/main/java/ispw/uniroma2/doctorhouse/auth/beans/DoctorBean.java
@@ -0,0 +1,13 @@
+package ispw.uniroma2.doctorhouse.auth.beans;
+
+public class DoctorBean {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}

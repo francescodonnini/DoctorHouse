e40015142d8c5359a04f945503fbb33b1ca78f7d
francesco donnini <donnini.francesco00@gmail.com>
2023-01-27 17:26:48 +0100

    Created PersistentLayerException

---
+++ src/main/java/ispw/uniroma2/doctorhouse/dao/exceptions/PersistentLayerException.java
@@ -0,0 +1,7 @@
+package ispw.uniroma2.doctorhouse.dao.exceptions;
+
+public class PersistentLayerException extends Exception {
+    public PersistentLayerException(Exception cause) {
+        super(cause);
+    }
+}
